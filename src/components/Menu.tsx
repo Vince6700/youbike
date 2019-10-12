cbd94c7f5ec441402d3f077bc83f0e24afd443c9
Vincent Racelle <vincent.racelle@gmail.com>
2019-10-12 07:55:48 +0200

    ios setup

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -22,7 +22,7 @@ const Menu: React.FunctionComponent<MenuProps> = ({ appPages }) => (
   <IonMenu contentId="main" type="overlay">
     <IonHeader>
       <IonToolbar>
-        <IonTitle>Menu</IonTitle>
+        <IonTitle>Youbike</IonTitle>
       </IonToolbar>
     </IonHeader>
     <IonContent>
