a43081c6b82cdb7463d2790b7d419e32daeb960c
Vincent Racelle <vincent.racelle@gmail.com>
2019-10-27 09:16:42 +0100

    redux thunk middleware implementation

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,6 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+//middleware
+import reduxThunk from 'redux-thunk';
 // import stores
 import reducer from './reducers/reducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
@@ -7,7 +9,11 @@ const rootReducer = combineReducers ({
     reducer: reducer,
 });
 
-const store = createStore(rootReducer, composeWithDevTools());
+const middleware = [
+    reduxThunk,
+]
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware)));
 
 export default store;
 
