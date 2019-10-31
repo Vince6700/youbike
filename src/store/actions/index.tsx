7948083041ca8ab0d0061b7f85a685c5c1048ffd
Vincent Racelle <vincent.racelle@gmail.com>
2019-10-31 12:49:44 +0100

    connecting the store to the component

--- src/store/actions/index.tsx
+++ src/store/actions/index.tsx
@@ -1,6 +1,34 @@
-export const FETCH_DATA = 'actions/FETCH_DATA';
+import store from '..'
 
-export const fetchData = (data : object) => ({
+export const FETCH_DATA = 'actions/FETCH_DATA';
+export const RECEIVED_DATA = 'actions/RECEIVED_DATA';
+export const RECEIVED_ERROR = 'actions/RECEIVED_ERROR';
+
+
+export const fetchData = () => ({
     type : 'FETCH_DATA',
-    events: data,
 });
+
+export const receivedData = (events : []) => ({
+    type : 'RECEIVED_DATA',
+    events 
+});
+
+export const receiveError = () => ({
+    type : 'RECEIVED_ERROR'
+});
+
+export const thunkActionCreator = () => {
+    store.dispatch(fetchData());
+    return function (dispatch : any, getState : any) {
+        return fetch('https://youbike-2a3ea.firebaseio.com/randos.json')
+            .then(data => data.json())
+            .then(data => {
+                if(data.message = 'Not Found') {
+                    throw new Error('No events found');
+                } else dispatch(receivedData)
+            })
+            .catch(err => dispatch(receiveError()));
+    } 
+}
+
--- src/store/reducers/reducer.ts
+++ src/store/reducers/reducer.ts
@@ -1,13 +1,30 @@
-import {FETCH_DATA} from '../actions/index';
+import {FETCH_DATA, RECEIVED_DATA, RECEIVED_ERROR} from '../actions/index';
 
-const initialState = { events: [] }
+const initialState = { events: [{'test':'test'}] }
 
 const reducer = (state = initialState, action = { type : '', events : []}) => {
     switch (action.type) {
         case FETCH_DATA: {
             return {
                 ...state,
+                isFetching: true,
+                isError: false,
+            }
+        }
+
+        case RECEIVED_DATA: {
+            return {
+                ...state,
                 events: action.events,
+                isFetching: false,
+                isError: false,
+            }
+        }
+        case RECEIVED_ERROR: {
+            return {
+                ...state,
+                isFetching: false,
+                isError: true,
             }
         }
         default:
