b3e6424e29719ac896ffec5d0644306b23787d2d
Vincent Racelle <vincent.racelle@gmail.com>
2019-11-18 13:17:06 +0100

    wip: getting events into state

--- src/store/actions/index.tsx
+++ src/store/actions/index.tsx
@@ -17,11 +17,12 @@ export const thunkActionCreator = () => {
     store.dispatch(fetchData());
     return function (dispatch : any, getState : any) {
         return fetch('https://youbike-2a3ea.firebaseio.com/randos.json')
-            .then(data => data.json())
+            .then(data => 
+                data.json())
             .then(jsonData => {
-                if(jsonData.message = 'Not Found') {
+                if(jsonData.message === 'Not Found') {
                     throw new Error('No events found');
-                } else dispatch(receivedData)
+                } else dispatch(receivedData(jsonData))
             })
             .catch(err => dispatch(receiveError()));
     } 
